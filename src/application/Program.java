a3e33e42088854f2270e683e9fc5210a362655d0
Flaesio <flaesio@yahoo.com.br>
2019-04-29 19:59:34 -0300

    Alterado

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,8 @@ public class Program {
     
     public static void main(String[] args) {
         System.out.println("bom dia!");
+        System.out.println("boa tarde!");
+        System.out.println("boa noite!");
     }
     
 }
